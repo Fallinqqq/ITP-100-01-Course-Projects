0dc1e67118d752aec88fa32107a91d0b93117f96
Grace K Foster <gkfoster15@gmail.com>
2021-10-29 19:01:18 -0400

    Adding unfinished EX 07 to project

---
+++ EX 8/courseinfo.py
@@ -0,0 +1,25 @@
+# Grace Foster
+# ITP 100-01
+# EXERCISE: 06
+# ageclass.py
+# ----------------------------------------------------------------
+# Write a program that asks the user for a course number and then displays the course's room number, instructor and meeting time.
+print("College Course Locator Program")
+course = str(input("Enter a course number:  "))
+print('---------------------------------------------------')
+print(f"The details for course {course} are: ")
+
+if course is not 
+
+print("room: ")
+room_values = {
+
+}
+print("Instructor:")
+instructors_value = {
+
+}
+print("meeting time: ")
+meeting_value = {
+
+}
---
+++ EX 8/dictionary example.py
@@ -0,0 +1,21 @@
+# Nested Dictionary outcome: states["Texas"]["flower"]
+# 'Bluebonnet'
+
+# states["Texas"]
+# {'capital': 'Austin', 'flower': 'Bluebonnet'}
+
+"""'states = {
+    "california": {
+        "capital": "Sacramento",
+        "flower": "California Poppy",
+    },
+    "New York": {
+        "capital": "Albany",
+        "flower": "Rose",
+
+    },
+    "Texas": {
+        "capital": "Austin",
+        "flower": "Bluebonnet",
+    },
+}''"""
