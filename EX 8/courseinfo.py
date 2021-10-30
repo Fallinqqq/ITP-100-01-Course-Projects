c6bdb17a270512fe30ba61f4da3ed87727ecd33d
Grace K Foster <gkfoster15@gmail.com>
2021-10-29 23:02:23 -0400

    adding: finished EX 07 to the project

--- EX 8/courseinfo.py
+++ EX 8/courseinfo.py
@@ -1,25 +1,41 @@
 # Grace Foster
 # ITP 100-01
-# EXERCISE: 06
-# ageclass.py
+# EXERCISE: 07
+# courseinfo.py
 # ----------------------------------------------------------------
-# Write a program that asks the user for a course number and then displays the course's room number, instructor and meeting time.
+room_dict = {
+    'CS101': '3004',
+    'CS102': '4501',
+    'CS103': '6755',
+    'NT110': '1244',
+    'CM241': '1411',
+}
+instructors_dict = {
+    'CS101': 'Haynes',
+    'CS102': 'Alvardo',
+    'CS103': 'Rich',
+    'NT110': 'Burkes',
+    'CM241': 'Lee',
+}
+meeting_dict = {
+    'CS101': '8:00 a.m.',
+    'CS102': '9:00 a.m.',
+    'CS103': '10:00 a.m.',
+    'NT110': '11:00 a.m.',
+    'CM241': '1:00 p.m.',
+}
+
 print("College Course Locator Program")
-course = str(input("Enter a course number:  "))
+course_number = str(input("Enter a course number:  "))
 print('---------------------------------------------------')
-print(f"The details for course {course} are: ")
 
-if course is not 
+if course_number not in room_dict:
+    print(f"{course_number} course number is invalid.")
+else:
+    print(f"The details for course {course_number} are: ")
+    print(f'Room: {room_dict [course_number]}')
+    print(f'Instructor: {instructors_dict [course_number]} ')
+    print(f'meeting time: {meeting_dict [course_number]} ')
 
-print("room: ")
-room_values = {
-
-}
-print("Instructor:")
-instructors_value = {
-
-}
-print("meeting time: ")
-meeting_value = {
-
-}
+print('---------------------------------------------------')
+print("program has terminated")
