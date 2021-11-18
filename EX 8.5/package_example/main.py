9179aa1ead75cf581e6cddc41dd1519b24c2130b
Grace K Foster <gkfoster15@gmail.com>
2021-11-18 13:54:36 -0500

    Adding:  Importing packages/naming

--- EX 8.5/package_example/main.py
+++ EX 8.5/package_example/main.py
@@ -1,7 +1,14 @@
 # main.py
 
-import mypackage.module1
-import mypackage.module2
+from mypackage import module1,module2
+# import mypackage.module1
+# import mypackage.module2
 
-mypackage.module1.greet("Pythonista")
-mypackage.module2.depart("Pythonista")
+# mypackage.module1.greet("Pythonista")
+# mypackage.module2.depart("Pythonista")
+
+from mypackage.module1 import greet as g
+from mypackage.module2 import depart as d
+
+g("me")
+d("me")
