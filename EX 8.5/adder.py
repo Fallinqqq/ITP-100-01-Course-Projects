77ee30062bc9c4bffc7eb6f9f2958c9930c5d73e
Grace K Foster <gkfoster15@gmail.com>
2021-11-18 13:22:36 -0500

    Adding: New folder for learning
    EX 8.5 is for learning reference. It is about importing packages and modules within one another.

---
+++ EX 8.5/adder.py
@@ -0,0 +1,11 @@
+# adder.py
+def add(x, y):
+    return x + y
+
+
+"""value = add(2, 2)
+print(value)"""
+
+
+def double(x):
+    return x + x
---
+++ EX 8.5/main.py
@@ -0,0 +1,8 @@
+# we are calling the module from adder.py to main.py
+import adder
+value = adder.add(2, 2)
+print(value)
+
+dbl = adder.double(4)
+print(dbl)
+
